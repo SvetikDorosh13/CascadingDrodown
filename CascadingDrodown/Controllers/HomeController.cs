9b0f30fc33062f06e4c0476940d9e3fd67cf67e6
abc44tester <59289377+abc44tester@users.noreply.github.com>
2020-07-28 15:59:23 -0500

    Add project files.

---
+++ CascadingDrodown/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CascadingDrodown.Models;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CascadingDrodown.Controllers
+{
+    public class HomeController : Controller
+    {
+        database_access_layer.db dbop = new database_access_layer.db();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            DataSet ds = dbop.GetCountry();
+            List<SelectListItem> list = new List<SelectListItem>();
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                list.Add(new SelectListItem { Text = dr["Country_name"].ToString(), Value = dr["Country_id"].ToString() });
+            }
+            ViewBag.Counrtylist = list;
+            return View();
+        }
+        public JsonResult GetStatelist(int cid)
+        {
+            DataSet ds = dbop.GetState(cid);
+            List<SelectListItem> list = new List<SelectListItem>();
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                list.Add(new SelectListItem { Text = dr["State_name"].ToString(), Value = dr["State_id"].ToString() });
+            }
+            return Json(list);
+        }
+        public JsonResult GetCitylist(int Sid)
+        {
+            DataSet ds = dbop.GetCity(Sid);
+            List<SelectListItem> list = new List<SelectListItem>();
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                list.Add(new SelectListItem { Text = dr["City_name"].ToString(), Value = dr["City_id"].ToString() });
+            }
+            return Json(list);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CascadingDrodown/database_access_layer/db.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace CascadingDrodown.database_access_layer
+{
+    public class db
+    {
+        SqlConnection con = new SqlConnection("Data Source=ADMINRG-N8EO0RN\\SQLEXPRESS;Initial Catalog=testdb;Integrated Security=True");
+
+        //Get Country List
+
+        public DataSet GetCountry()
+        {
+            SqlCommand com = new SqlCommand("Sp_Country", con);
+            com.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+        public DataSet GetState(int cid)
+        {
+            SqlCommand com = new SqlCommand("Sp_State", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Country_id", cid);
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+        public DataSet GetCity(int Sid)
+        {
+            SqlCommand com = new SqlCommand("Sp_City", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@State_id", Sid);
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+    }
+}
